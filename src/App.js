36078b5286b639d902dcdde811b4d4b1acae021c
Harrolee <Harrolee@users.noreply.github.com>
2021-02-19 15:57:27 -0800

    change App.js a lil

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit FOUND IT!! <code>src/App.js</code> and save to reload.
+          changed on the feature branch <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
